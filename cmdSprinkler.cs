6108e9268d8956c24677043bd2f780a512df20af
ThomasEFlaherty <ThomasEFlaherty@LUSAD_LAPTOP>
2025-10-03 16:59:59 -0500

    in progress

--- cmdSprinkler.cs
+++ cmdSprinkler.cs
@@ -37,6 +37,26 @@ namespace ConvertSpecLevel
             Wall garageWall = SelectWall(uidoc, "Select front garage wall.");
             if (garageWall == null) return Result.Cancelled;
 
+            // get garage wall exterior face point
+            XYZ facePoint = GetWallExteriorStructuralFace(garageWall);
+
+            // null check
+            if (facePoint == null)
+            {
+                Utils.TaskDialogError("Error", "Sprinkler Outlet", "Could not determine garage wall exterior structural face.");
+                return Result.Failed;
+            }
+
+            // Compute offset in direction of garage wall's orientation (which points OUT)
+            double offsetFeet = UnitUtils.ConvertToInternalUnits(60, UnitTypeId.Inches);
+
+            // reverse direction to go IN from garage face
+            XYZ garageToOutletDir = -garageWall.Orientation; // Going inward from garage face
+            XYZ offsetPoint = facePoint + (garageToOutletDir * offsetFeet);
+
+            // create variable for outlet insertion point
+            XYZ outletPoint = new XYZ(offsetPoint.X, offsetPoint.Y, 0);
+
 
 
 
@@ -48,6 +68,11 @@ namespace ConvertSpecLevel
 
             return Result.Succeeded;
 
+        }
+
+        private XYZ GetWallExteriorStructuralFace(Wall garageWall)
+        {
+            throw new NotImplementedException();
         }
 
         private Wall SelectWall(UIDocument uidoc, string prompt)
