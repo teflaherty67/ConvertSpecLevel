8319ea3346939ea8fd3c20563484c8d6e14833bb
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2025-10-20 12:39:39 -0500

    revert

--- ConvertSpecLevel/frmConvertSpecLevel.xaml.cs
+++ ConvertSpecLevel/frmConvertSpecLevel.xaml.cs
@@ -26,9 +26,7 @@ namespace ConvertSpecLevel
         // properties for form return values
         public string SelectedClientName { get; private set; }
         public bool IsCompleteHomePlus { get; private set; }
-        public Reference SelectedOutlet { get; set; }
-        public Reference SelectedOutletWall { get; set; }
-        public Reference SelectedGarageWall { get; set; }
+
 
 
         #region Constructors
@@ -69,7 +67,7 @@ namespace ConvertSpecLevel
 
             // Set the default selection to the first client in the list (Central Texas)
             if (cmbClient.Items.Count > 0)
-                cmbClient.SelectedIndex = 0;           
+                cmbClient.SelectedIndex = 0;
         }
 
         #endregion
@@ -91,7 +89,7 @@ namespace ConvertSpecLevel
             {
                 return rbCompleteHomePlus.Content.ToString();
             }
-        }       
+        }
 
         #endregion
 
@@ -100,42 +98,6 @@ namespace ConvertSpecLevel
         private void SpecLevel_Changed(object sender, RoutedEventArgs e)
         {
             // UpdateDynamicContent();
-        }
-
-        private void SelectOutlet()
-        {
-            try
-            {
-                this.Hide();
-
-                // get all views with Electrical in the name & associated with the First Floor
-                List<View> firstFloorElecViews = Utils.GetAllViewsByNameContainsAndAssociatedLevel(CurDoc, "Electrical", "First Floor");
-
-                // get the first view in the list
-                if (firstFloorElecViews.Any())
-                {
-                    // set that view as the active view
-                    UIDoc.ActiveView = firstFloorElecViews.First();
-
-                    // prompt the user to select the outlet to delete
-                    SelectedOutlet = UIDoc.Selection.PickObject(ObjectType.Element, new OutletSelectionFilter(), "Select sprinkler outlet to remove");
-                }
-                else
-                {
-                    // notify the user that no Electrical views were found
-                    Utils.TaskDialogWarning("Warning", "Spec Conversion", "No Electrical views found associated with the First Floor.");
-                }
-
-                btnDynamicRow.Content = "Selected";
-                btnDynamicRow.IsEnabled = true;
-
-                this.Show();
-            }
-            catch (Exception ex)
-            {
-                // notify the user of the error
-                Utils.TaskDialogError("Error", "Spec Conversion", $"Error selecting outlet: {ex.Message}");
-            }
         }
 
         #endregion
@@ -174,30 +136,5 @@ namespace ConvertSpecLevel
         }
 
         #endregion
-
-        internal class OutletSelectionFilter : ISelectionFilter
-        {
-            public bool AllowElement(Element elem)
-            {
-                // Check if element is an electrical fixture
-                if (elem.Category != null && elem.Category.Name == "Electrical Fixtures")
-                {
-                    // Cast to FamilyInstance to access the Symbol
-                    if (elem is FamilyInstance familyInstance)
-                    {
-                        // Check if the type name contains "Outlet" or "Sprinkler"
-                        string typeName = familyInstance.Symbol.Name;
-                        return typeName.Contains("Outlet", StringComparison.OrdinalIgnoreCase) ||
-                               typeName.Contains("Sprinkler", StringComparison.OrdinalIgnoreCase);
-                    }
-                }
-                return false;
-            }
-            public bool AllowReference(Reference reference, XYZ position)
-            {
-                // Allow all references
-                return true;
-            }
-        }
     }
 }
